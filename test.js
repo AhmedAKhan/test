5c3b9967cbc2f51133fecee38839512bd0c97be5
Ahmed <khanaahmed10@gmail.com>
2017-06-29 15:59:08 -0400

    made a dockerode test

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 
 
+var Docker = require('dockerode');
+var docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+
 describe("this is a tester start", function(){
   it("this is test 1", function(done){
-    console.log("tested 1")
-    done()
+    console.log("going to list the containers")
+    docker.listContainers(function (err, containers) {
+      console.log("errors are ", err)
+      console.log("containers are: ", containers)
+      done()
+    });
   })
 })
 
